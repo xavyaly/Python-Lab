991cfabe4f29ea303686ea954f2cb23224b340ec
Javed Alam <wellboy.alam13@gmail.com>
2017-11-04 13:26:00 +0530

    Update Test2_Sol.py

--- Scripts/Test2_Sol.py
+++ Scripts/Test2_Sol.py
@@ -1,1 +1,16 @@
+#!usr/bin/python
+
+>>> import keyword
+>>> keyword.kwlist
+['and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'exec', 'finally', 
+ 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'print', 'raise', 'return', 
+ 'try', 'while', 'with', 'yield']
+>>> 
+>>> 
+>>> from keyword import kwlist
+>>> print(kwlist)
+['and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'exec', 'finally', 
+ 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'print', 'raise', 'return', 
+ 'try', 'while', 'with', 'yield']
+>>> 
 
