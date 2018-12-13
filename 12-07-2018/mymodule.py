8882c4555e1e29405b4f504337a7e1e7cec0f6d9
xavyaly <wellboy.alam13@gmail.com>
2018-12-13 12:25:17 +0530

    deleted all labs

--- 12-07-2018/mymodule.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-def greeting(name):
-	print("Hello " + name )
--- 12-07-2018/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-#OPEN A NEW FILE
-
-with open("/Users/javedalam/Documents/MyGit/Python/12-07-2018/new-file1.txt",mode='w') as f:
-	f.write("My first line\n")
-	f.write("This is the second line\n\n")
-	f.write("This is the third line\n")
-
