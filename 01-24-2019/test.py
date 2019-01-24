db8016200fcf5775bd0b027989c4274c36041804
xavyaly <wellboy.alam13@gmail.com>
2019-01-24 10:39:09 +0530

    Python-End

---
+++ 01-24-2019/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#IMPORT A MODULE
+
+import os
+import sys
+
+#CREATE A RANDOM LIST
+randomList = ['z',0,4]
+
+#CODE
+for i in randomList:
+	try:
+		print("The entry is: ", i)
+		r = 1/int(i)
+		break
+	except:
+		print("Oops", sys.exc_info()[0],"Occured")
+		print("Next Entry!!!")
+
+print("The reciprocal od: ",i,"is",r)
