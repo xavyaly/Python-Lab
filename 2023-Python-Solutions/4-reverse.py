91d7fc0499784da9746113f69e5fcf824783e12b
xavyaly <wellboy.alam13@gmail.com>
2023-08-02 20:56:24 +0530

    reverse

--- 2023-Python-Solutions/4-reverse.py
+++ 2023-Python-Solutions/4-reverse.py
@@ -5,3 +5,14 @@ def reverse_a_string(str):
 
 str = input("Enter a string: ")
 print(reverse_a_string(str))
+
+# Execution Output
+
+# python3 4-reverse.py 
+# Enter a string: abc
+# cba
+
+# python3 4-reverse.py 
+# Enter a string: agfhfkjhsd
+# dshjkfhfga
+
